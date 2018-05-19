3435e5921ffb2fa63260b1e323541609b69dce87
Alessandro Zotta <zotta.ale@gmail.com>
2018-05-19 15:09:04 +0200

    data and graph style
    color and labels

--- oggi.js
+++ oggi.js
@@ -7,17 +7,21 @@ let svg = d3.selectAll("body")
 .attr("width", width - margin)
 .attr("height", height - margin)
 .style("background", "#fbfbfb")
-.style("border", "1px solid #919191")
+//.style("border", "1px solid #919191")
 
 //scales
 let x = d3.scaleLinear()
-.range([0, width])
+.range([20, width])
 
 let y = d3.scaleLinear()
-.range([height, 0])
+.range([height - 50, 0])
 
 let size = d3.scaleSqrt()
-.range([0,30])
+.range([1,30])
+
+let color = d3.scaleLinear()
+.interpolate(d3.interpolateHcl)
+.range([d3.rgb("#ff0000"), d3.rgb("#0000ff")])
 
 d3.csv("honeyproduction.csv", function(error, data) {
 	if (error) throw error
@@ -34,7 +38,11 @@ d3.csv("honeyproduction.csv", function(error, data) {
 		return +d.numcol
 	}))
 
-	svg.selectAll("svg")
+	color.domain(d3.extent(data, d => {
+		return +d.year
+	}))
+
+svg.selectAll("svg")
 	.data(data)
 	.enter()
 	.append("rect")
@@ -43,7 +51,18 @@ d3.csv("honeyproduction.csv", function(error, data) {
 	.attr("x", d => { return x(d.priceperlb)})
 	.attr("y", d => { return y(d.totalprod)})
 	.style("opacity", 0.5)
-	.style("fill", "#f6d365")
+	.style("fill", d => { return color(d.year)})
+
+svg.selectAll("text")
+	.data(data)
+	.enter()
+	.append("text")
+	.attr("x", d => { return x(d.priceperlb)})
+	.attr("y", d => { return y(d.totalprod) - 2})
+	.text(d => { return d.state})
+	.classed("label", true)
+	.style("fill", d => { return color(d.year)})
+
 
 	//console.log(JSON.stringify(data, null, "\t"));
 
